9325683cff05968ba0549fda9a24c7395ac51b57
Lucas Hype do T.I <70492860+Itakasz@users.noreply.github.com>
2020-08-30 21:40:03 -0300

    Aulas de Comp
    Aula 1 - printf
    Aula 2 - (char, int, float, double)

---
+++ Aula 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Dalle cool D foss !\n");
+    return 0;
+}
---
+++ Aula 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    tipo nome;
+    char //caracteres
+    char letra = 'a';
+
+    int // inteiros
+    int nro = 10;
+
+    float // numeros nao inteiros
+    float n = 5.25;
+
+    double // numeros com varias casas decimais
+    double d = 5.0e5;
+
+
+    printf("De novo programando menó?!\n");
+    return 0;
+}
